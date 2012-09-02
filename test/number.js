5ceb214c61eedd3434cf392aea024ff70ddb92e5
Paul Miller <paul@paulmillr.com>
2012-09-02 14:56:29 +0300

    Import number tests from mozilla-central.

--- test/number.js
+++ test/number.js
@@ -14,7 +14,7 @@ describe('Number', function() {
     it('should has max integer', function() {
       expect(Number.MAX_INTEGER).to.equal(9007199254740992);
     });
-    
+
     it('should has epsilon', function() {
       expect(Number.EPSILON).to.equal(2.220446049250313e-16);
     });
@@ -36,8 +36,34 @@ describe('Number', function() {
     it('should work', function() {
       integers.map(Number.isFinite).forEach(expectToBeOk);
       infinities.map(Number.isFinite).forEach(expectToNotBeOk);
+      expect(Number.isFinite(Infinity)).to.not.be.ok
+      expect(Number.isFinite(-Infinity)).to.not.be.ok;
+      expect(Number.isFinite(NaN)).to.not.be.ok;
+      expect(Number.isFinite(4)).to.be.ok;
+      expect(Number.isFinite(4.5)).to.be.ok;
+      expect(Number.isFinite('hi')).to.not.be.ok;
+      expect(Number.isFinite('1.3')).to.not.be.ok;
+      expect(Number.isFinite('51')).to.not.be.ok;
+      expect(Number.isFinite(0)).to.be.ok;
+      expect(Number.isFinite(-0)).to.be.ok;
+      expect(Number.isFinite({
+        valueOf: function() { return 3; }
+      })).to.not.be.ok;
+      expect(Number.isFinite({
+        valueOf: function() { return 0/0; }
+      })).to.not.be.ok;
+      expect(Number.isFinite({
+        valueOf: function() { throw 17; }
+      })).to.not.be.ok;
+      expect(Number.isFinite({
+        toString: function() { throw 17; }
+      })).to.not.be.ok;
+      expect(Number.isFinite({
+        valueOf: function() { throw 17; },
+        toString: function() { throw 42; }
+      })).to.not.be.ok;
     });
-    
+
     it('should not be confused by type coercion', function() {
       nonNumbers.map(Number.isFinite).forEach(expectToNotBeOk);
     });
@@ -46,35 +72,92 @@ describe('Number', function() {
   describe('Number.isInteger()', function() {
     it('should be truthy on integers', function() {
       integers.map(Number.isInteger).forEach(expectToBeOk);
-    });
-
-    it('should not be confused by type coercion', function() {
-      nonIntegers.concat(
-        infinities, nonNumbers
-      ).map(Number.isInteger).forEach(expectToNotBeOk);
+      expect(Number.isInteger(4)).to.be.ok;
+      expect(Number.isInteger(4.)).to.be.ok;
+      expect(Number.isInteger(4.2)).to.not.be.ok;
+      expect(Number.isInteger(0.)).to.be.ok;
+      expect(Number.isInteger(-0.)).to.be.ok;
+      expect(Number.isInteger(Infinity)).to.not.be.ok;
+      expect(Number.isInteger(-Infinity)).to.not.be.ok;
+      expect(Number.isInteger(NaN)).to.not.be.ok;
+      expect(Number.isInteger(true)).to.not.be.ok;
+      expect(Number.isInteger(false)).to.not.be.ok;
+      expect(Number.isInteger('str')).to.not.be.ok;
+      expect(Number.isInteger({})).to.not.be.ok;
+      expect(Number.isInteger({
+        valueOf: function() { return 3; }
+      })).to.not.be.ok;
+      expect(Number.isInteger({
+        valueOf: function() { return 0/0; }
+      })).to.not.be.ok;
+      expect(Number.isInteger({
+        valueOf: function() { throw 17; }
+      })).to.not.be.ok;
+      expect(Number.isInteger({
+        toString: function() { throw 17; }
+      })).to.not.be.ok;
+      expect(Number.isInteger({
+        valueOf: function() { throw 17; },
+        toString: function() { throw 42; }
+      })).to.not.be.ok;
     });
   });
 
   describe('Number.isNaN()', function() {
     it('should be truthy only on NaN', function() {
-      expect(Number.isNaN(NaN)).to.be.ok;
       integers.concat(nonIntegers).map(Number.isNaN).forEach(expectToNotBeOk);
-    });
-
-    it('should not be confused by type coercion', function() {
       nonNumbers.map(Number.isNaN).forEach(expectToNotBeOk);
+      expect(Number.isNaN(NaN)).to.be.ok;
+      expect(Number.isNaN(0/0)).to.be.ok;
+      expect(Number.isNaN(Number('NaN'))).to.be.ok;
+      expect(Number.isNaN(4)).to.not.be.ok;
+      expect(Number.isNaN(4.5)).to.not.be.ok;
+      expect(Number.isNaN('hi')).to.not.be.ok;
+      expect(Number.isNaN('1.3')).to.not.be.ok;
+      expect(Number.isNaN('51')).to.not.be.ok;
+      expect(Number.isNaN(0)).to.not.be.ok;
+      expect(Number.isNaN(-0)).to.not.be.ok;
+      expect(Number.isNaN({valueOf: function() { return 3; }})).to.not.be.ok;
+      expect(Number.isNaN({valueOf: function() { return 0/0; }})).to.not.be.ok;
+      expect(Number.isNaN({valueOf: function() { throw 17; } })).to.not.be.ok;
+      expect(Number.isNaN({toString: function() { throw 17; } })).to.not.be.ok;
+      expect(Number.isNaN({
+        valueOf: function() { throw 17; },
+        toString: function() { throw 42; }
+      })).to.not.be.ok;
     });
   });
 
   describe('Number.toInteger()', function() {
     it('should convert things to integer value', function() {
-      integers.concat(infinities).forEach(function(item) {
-        expect(Number.toInteger(item)).to.equal(item);
-      });
-      [[1.5, 1], [-1.5, -1], [1/3, 0], [NaN, 0], ['str', 0]
-        ].forEach(function(item) {
-        expect(Number.toInteger(item[0])).to.equal(item[1]);
-      });
+      expect(Number.toInteger(4)).to.equal(4);
+      expect(Number.toInteger(4.)).to.equal(4);
+      expect(Number.toInteger(4.3)).to.equal(4);
+      expect(Number.toInteger(-4)).to.equal(-4);
+      expect(Number.toInteger(-4.)).to.equal(-4);
+      expect(Number.toInteger(-4.3)).to.equal(-4);
+      expect(Number.toInteger(0.)).to.equal(0.);
+      expect(Number.toInteger(-0.)).to.equal(-0.);
+      expect(Number.toInteger(Infinity)).to.equal(Infinity);
+      expect(Number.toInteger(-Infinity)).to.equal(-Infinity);
+      expect(Number.toInteger(NaN)).to.equal(0);
+      expect(Number.toInteger(null)).to.equal(0);
+      expect(Number.toInteger(undefined)).to.equal(0);
+      expect(Number.toInteger(true)).to.equal(1);
+      expect(Number.toInteger(false)).to.equal(0);
+      expect(Number.toInteger({
+        valueOf: function() { return 4; }
+      })).to.equal(4);
+      expect(Number.toInteger({
+        valueOf: function() { return 4.3; }
+      })).to.equal(4);
+      expect(Number.toInteger({
+        valueOf: function() { return '4'; }
+      })).to.equal(4);
+      expect(Number.toInteger({
+        valueOf: function() { return {};}
+      })).to.equal(0);
+      expect(Number.toInteger()).to.equal(0);
     });
   });
 
