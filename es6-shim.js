2f4d976a3c26a54ee44208a7dcc11767256c5369
Paul Miller <paul@paulmillr.com>
2012-09-02 12:08:30 +0300

    Import str tests from mozilla-central.

--- es6-shim.js
+++ es6-shim.js
@@ -44,17 +44,88 @@
       return half + half;
     },
 
-    startsWith: function(substring) {
-      return this.lastIndexOf(substring, 0) === 0;
+    startsWith: function(searchString) {
+      var position = arguments[1];
+
+      // 1. Let searchStr be ToString(searchString).
+      var searchStr = searchString.toString();
+
+      // 2. ReturnIfAbrupt(searchStr).
+
+      // 3. Let S be the result of calling ToString,
+      // giving it the this value as its argument.
+      var s = this.toString();
+
+      // 4. ReturnIfAbrupt(S).
+
+      // 5. Let pos be ToInteger(position).
+      // (If position is undefined, this step produces the value 0).
+      var pos = (position === undefined) ? 0 : Number.toInteger(position);
+      // 6. ReturnIfAbrupt(pos).
+
+      // 7. Let len be the number of elements in S.
+      var len = s.length;
+
+      // 8. Let start be min(max(pos, 0), len).
+      var start = Math.min(Math.max(pos, 0), len);
+
+      // 9. Let searchLength be the number of elements in searchString.
+      var searchLength = searchString.length;
+
+      // 10. If searchLength+start is greater than len, return false.
+      if ((searchLength + start) > len) return false;
+
+      // 9. If the searchLength sequence of elements of S starting at
+      // start is the same as the full element sequence of searchString,
+      // return true.
+      var index = ''.indexOf.call(s, searchString, start);
+      return index === start;
     },
 
-    endsWith: function(substring) {
-      var startFrom = this.length - String(substring).length;
-      return startFrom >= 0 && this.indexOf(substring, startFrom) === startFrom;
+    endsWith: function(searchString) {
+      var endPosition = arguments[1];
+
+      // ReturnIfAbrupt(CheckObjectCoercible(this value)).
+      // Let S be the result of calling ToString, giving it the this value as its argument.
+      // ReturnIfAbrupt(S).
+      var s = this.toString();
+
+      // Let searchStr be ToString(searchString).
+      // ReturnIfAbrupt(searchStr).
+      var searchStr = searchString.toString();
+
+      // Let len be the number of elements in S.
+      var len = s.length;
+
+      // If endPosition is undefined, let pos be len, else let pos be ToInteger(endPosition).
+      // ReturnIfAbrupt(pos).
+      var pos = (endPosition === undefined) ?
+        len :
+        Number.toInteger(endPosition);
+
+      // Let end be min(max(pos, 0), len).
+      var end = Math.min(Math.max(pos, 0), len);
+
+      // Let searchLength be the number of elements in searchString.
+      var searchLength = searchString.length;
+
+      // Let start be end - searchLength.
+      var start = end - searchLength;
+
+      // If start is less than 0, return false.
+      if (start < 0) return false;
+
+      // If the searchLength sequence of elements of S starting at start is the same as the full element sequence of searchString, return true.
+      // Otherwise, return false.
+      var index = ''.indexOf.call(s, searchString, start);
+      return index === start;
     },
 
-    contains: function(substring) {
-      return this.indexOf(substring) !== -1;
+    contains: function(searchString) {
+      var position = arguments[1];
+
+      // Somehow this trick makes method 100% compat with the spec.
+      return ''.indexOf.call(this, searchString, position) !== -1;
     }
   });
 
--- test/string.js
+++ test/string.js
@@ -8,32 +8,180 @@ describe('String', function() {
   describe('#startsWith()', function() {
     it('should be truthy on correct results', function() {
       expect('test'.startsWith('te')).to.be.ok;
-    });
-
-    it('should be falsy on incorrect results', function() {
       expect('test'.startsWith('st')).to.not.be.ok;
       expect(''.startsWith('/')).to.not.be.ok;
       expect('#'.startsWith('/')).to.not.be.ok;
       expect('##'.startsWith('///')).to.not.be.ok;
+
+      expect('abc'.startsWith('abc')).to.be.ok;
+      expect('abcd'.startsWith('abc')).to.be.ok;
+      expect('abc'.startsWith('a')).to.be.ok;
+      expect('abc'.startsWith('abcd')).to.not.be.ok;
+      expect('abc'.startsWith('bcde')).to.not.be.ok;
+      expect('abc'.startsWith('b')).to.not.be.ok;
+      expect('abc'.startsWith('abc', 0)).to.be.ok;
+      expect('abc'.startsWith('bc', 0)).to.not.be.ok;
+      expect('abc'.startsWith('bc', 1)).to.be.ok;
+      expect('abc'.startsWith('c', 1)).to.not.be.ok;
+      expect('abc'.startsWith('abc', 1)).to.not.be.ok;
+      expect('abc'.startsWith('c', 2)).to.be.ok;
+      expect('abc'.startsWith('d', 2)).to.not.be.ok;
+      expect('abc'.startsWith('dcd', 2)).to.not.be.ok;
+      expect('abc'.startsWith('a', 42)).to.not.be.ok;
+      expect('abc'.startsWith('a', Infinity)).to.not.be.ok;
+      expect('abc'.startsWith('a', NaN)).to.be.ok;
+      expect('abc'.startsWith('b', NaN)).to.not.be.ok;
+      expect('abc'.startsWith('ab', -43)).to.be.ok;
+      expect('abc'.startsWith('ab', -Infinity)).to.be.ok;
+      expect('abc'.startsWith('bc', -42)).to.not.be.ok;
+      expect('abc'.startsWith('bc', -Infinity)).to.not.be.ok;
+      var myobj = {
+        toString: function() {return 'abc';},
+        startsWith: String.prototype.startsWith
+      };
+      expect(myobj.startsWith('abc')).to.be.ok;
+      expect(myobj.startsWith('bc')).to.not.be.ok;
+
+      var gotStr = false, gotPos = false;
+
+      myobj = {
+        toString: function() {
+          expect(gotPos).to.not.be.ok;
+          gotStr = true;
+          return 'xyz';
+        },
+        startsWith: String.prototype.startsWith
+      };
+      var idx = {
+        valueOf: function() {
+          expect(gotStr).to.be.ok;
+          gotPos = true;
+          return 42;
+        }
+      };
+      myobj.startsWith('elephant', idx);
+      expect(gotPos).to.be.ok;
     });
   });
 
   describe('#endsWith()', function() {
     it('should be truthy on correct results', function() {
       expect('test'.endsWith('st')).to.be.ok;
-    })
-
-    it('should be falsy on incorrect results', function() {
       expect('test'.endsWith('te')).to.not.be.ok;
       expect(''.endsWith('/')).to.not.be.ok;
       expect('#'.endsWith('/')).to.not.be.ok;
       expect('##'.endsWith('///')).to.not.be.ok;
+
+      expect('abc'.endsWith('abc')).to.be.ok;
+      expect('abcd'.endsWith('bcd')).to.be.ok;
+      expect('abc'.endsWith('c')).to.be.ok;
+      expect('abc'.endsWith('abcd')).to.not.be.ok;
+      expect('abc'.endsWith('bbc')).to.not.be.ok;
+      expect('abc'.endsWith('b')).to.not.be.ok;
+      expect('abc'.endsWith('abc', 3)).to.be.ok;
+      expect('abc'.endsWith('bc', 3)).to.be.ok;
+      expect('abc'.endsWith('a', 3)).to.not.be.ok;
+      expect('abc'.endsWith('bc', 3)).to.be.ok;
+      expect('abc'.endsWith('a', 1)).to.be.ok;
+      expect('abc'.endsWith('abc', 1)).to.not.be.ok;
+      expect('abc'.endsWith('b', 2)).to.be.ok;
+      expect('abc'.endsWith('d', 2)).to.not.be.ok;
+      expect('abc'.endsWith('dcd', 2)).to.not.be.ok;
+      expect('abc'.endsWith('a', 42)).to.not.be.ok;
+      expect('abc'.endsWith('bc', Infinity)).to.be.ok;
+      expect('abc'.endsWith('a', Infinity)).to.not.be.ok;
+      expect('abc'.endsWith('bc', undefined)).to.be.ok;
+      expect('abc'.endsWith('bc', -43)).to.not.be.ok;
+      expect('abc'.endsWith('bc', -Infinity)).to.not.be.ok;
+      expect('abc'.endsWith('bc', NaN)).to.not.be.ok;
+
+      var myobj = {
+        toString: function() {return 'abc'},
+        endsWith: String.prototype.endsWith
+      };
+      expect(myobj.endsWith('abc')).to.be.ok;
+      expect(myobj.endsWith('ab')).to.not.be.ok;
+      var gotStr = false, gotPos = false;
+
+      myobj = {
+        toString: function() {
+          expect(gotPos).to.not.be.ok;
+          gotStr = true;
+          return 'xyz';
+        },
+        endsWith : String.prototype.endsWith
+      };
+      var idx = {
+        valueOf: function () {
+          expect(gotStr).to.be.ok;
+          gotPos = true;
+          return 42;
+        }
+      };
+      myobj.endsWith('elephant', idx);
+      expect(gotPos).to.be.ok;
     });
   });
 
   describe('#contains()', function() {
     it('should be truthy on correct results', function() {
       expect('test'.contains('es')).to.be.ok;
+      expect('abc'.contains('a')).to.be.ok;
+      expect('abc'.contains('b')).to.be.ok;
+      expect('abc'.contains('abc')).to.be.ok;
+      expect('abc'.contains('bc')).to.be.ok;
+      expect('abc'.contains('d')).to.not.be.ok;
+      expect('abc'.contains('abcd')).to.not.be.ok;
+      expect('abc'.contains('ac')).to.not.be.ok;
+      expect('abc'.contains('abc', 0)).to.be.ok;
+      expect('abc'.contains('bc', 0)).to.be.ok;
+      expect('abc'.contains('de', 0)).to.not.be.ok;
+      expect('abc'.contains('bc', 1)).to.be.ok;
+      expect('abc'.contains('c', 1)).to.be.ok;
+      expect('abc'.contains('a', 1)).to.not.be.ok;
+      expect('abc'.contains('abc', 1)).to.not.be.ok;
+      expect('abc'.contains('c', 2)).to.be.ok;
+      expect('abc'.contains('d', 2)).to.not.be.ok;
+      expect('abc'.contains('dcd', 2)).to.not.be.ok;
+      expect('abc'.contains('a', 42)).to.not.be.ok;
+      expect('abc'.contains('a', Infinity)).to.not.be.ok;
+      expect('abc'.contains('ab', -43)).to.be.ok;
+      expect('abc'.contains('cd', -42)).to.not.be.ok;
+      expect('abc'.contains('ab', -Infinity)).to.be.ok;
+      expect('abc'.contains('cd', -Infinity)).to.not.be.ok;
+      expect('abc'.contains('ab', NaN)).to.be.ok;
+      expect('abc'.contains('cd', NaN)).to.not.be.ok;
+
+      var myobj = {
+        toString: function() {return 'abc';},
+        contains: String.prototype.contains
+      };
+
+      expect(myobj.contains('abc')).to.be.ok;
+      expect(myobj.contains('cd')).to.not.be.ok;
+
+      var gotStr = false, gotPos = false;
+
+      myobj = {
+        toString: function() {
+          expect(gotPos).to.not.be.ok;
+          gotStr = true;
+          return 'xyz';
+        },
+
+        contains: String.prototype.contains
+      };
+
+      var idx = {
+        valueOf: function() {
+          expect(gotStr).to.be.ok;
+          gotPos = true;
+          return 42;
+        }
+      };
+
+      myobj.contains('elephant', idx);
+      expect(gotPos).to.be.ok;
     });
 
     it('should be falsy on incorrect results', function() {
