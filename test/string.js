eac79a066f41e0f7e85d776cf61c5a889f77b934
Jordan Harband <jharband@twitter.com>
2013-03-24 16:41:39 -0500

    Adding tests for String.fromCodePoint and String#codePointAt

--- test/string.js
+++ test/string.js
@@ -201,4 +201,51 @@ describe('String', function() {
       expect('test'.contains('1290')).to.not.be.ok;
     });
   });
+
+  describe('.fromCodePoint()', function() {
+    it('throws a RangeError', function() {
+      var invalidValues = [
+        'abc',
+        {},
+        -1,
+        0x10FFFF + 1
+      ];
+      invalidValues.forEach(function(value) {
+        expect(function() { return String.fromCodePoint(value); }).to.throw(RangeError);
+      });
+    });
+
+    it('returns the empty string with no args', function() {
+      expect(String.fromCodePoint()).to.equal('');
+    });
+
+    it('has a length of zero', function() {
+      expect(String.fromCodePoint.length).to.equal(0);
+    });
+
+    it('works', function() {
+      var codePoints = [];
+      var chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789…?!';
+      for (var i = 0; i < chars.length; ++i) {
+        codePoints.push(chars.charCodeAt(i));
+        expect(String.fromCodePoint(chars.charCodeAt(i))).to.equal(chars[i]);
+      }
+      expect(String.fromCodePoint.apply(String, codePoints)).to.equal(chars);
+    });
+  });
+
+  describe('#codePointAt()', function() {
+    it('works', function() {
+      var str = 'abc';
+      expect(str.codePointAt(0)).to.equal(97);
+      expect(str.codePointAt(1)).to.equal(98);
+      expect(str.codePointAt(2)).to.equal(99);
+    });
+
+    it('returns undefined when pos is negative or too large', function() {
+      var str = 'abc';
+      expect(str.codePointAt(-1)).to.be.undefined;
+      expect(str.codePointAt(str.length)).to.be.undefined;
+    });
+  });
 });
