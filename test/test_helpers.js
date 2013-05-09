af0545e965ef266cb233897a966a7f38718caec6
Jordan Harband <ljharb@gmail.com>
2013-05-09 11:37:11 -0700

    Adding a "theSameSet" assertion to compare two arrays with strict equality.

--- test/test_helpers.js
+++ test/test_helpers.js
@@ -1,2 +1,28 @@
-expect = require('chai').expect;
+var chai = require('chai'),
+    Assertion = chai.Assertion;
+
+expect = chai.expect;
+
+Assertion.addMethod('theSameSet', function (otherArray) {
+    var array = this._obj;
+
+    expect(Array.isArray(array)).to.equal(true);
+    expect(Array.isArray(otherArray)).to.equal(true);
+
+    var diff = array.filter(function (value) {
+        return !otherArray.some(function (otherValue) {
+            var areBothNaN = typeof value === 'number' && typeof otherValue === 'number' && value !== value && otherValue !== otherValue;
+            return areBothNaN || value === otherValue;
+        });
+    });
+
+    this.assert(
+        diff.length === 0,
+        "expected #{this} to be equal to #{exp} (as sets, i.e. no order)",
+        array,
+        otherArray
+    );
+});
+
 require('../');
+
