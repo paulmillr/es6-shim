d019e686c101dfc880a2163e52564146cc9ae106
Paul Miller <paul@paulmillr.com>
2013-05-17 08:55:38 +0300

    Improve code style.

--- test/browser-onload.js
+++ test/browser-onload.js
@@ -1,15 +1,16 @@
 window.onload = function() {
   window.testsPassed = null;
-  function handleResults(runner) {
-    if (runner.stats.end)
-      window.testsPassed = runner.stats.failures === 0;
+  var handleResults = function(runner) {
+    if (runner.stats.end) {
+      window.testsPassed = (runner.stats.failures === 0);
+    }
     runner.on('fail', function(test, err){
       window.testsPassed = false;
     });
     runner.on('end', function(){
-      window.testsPassed = window.testsPassed == null;
+      window.testsPassed = (window.testsPassed == null);
     });
     return runner;
-  }
+  };
   handleResults(mocha.run());
 };
