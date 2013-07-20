b0745de6dd2803742deeafe9ec8244598af6940c
Daniel Hug <daniel@hugwebdesign.com>
2013-07-20 15:15:16 -0700

    Redo startsWith/endsWith tests

--- test/string.js
+++ test/string.js
@@ -58,16 +58,12 @@ describe('String', function() {
       expect('abc'.startsWith('ab', -Infinity)).to.be.ok;
       expect('abc'.startsWith('bc', -42)).to.not.be.ok;
       expect('abc'.startsWith('bc', -Infinity)).to.not.be.ok;
-      expect((function() {
-        try { ''.startsWith.call(null, 'nu'); }
-        catch(e) { return true; }
-        return false;
-      })()).to.be.ok;
-      expect((function() {
-        try { ''.startsWith.call(undefined, 'un'); }
-        catch(e) { return true; }
-        return false;
-      })()).to.be.ok;
+      expect(function() {
+        ''.startsWith.call(null, 'nu');
+      }).to.throw(TypeError);
+      expect(function() {
+        ''.startsWith.call(undefined, 'un');
+      }).to.throw(TypeError);
       var myobj = {
         toString: function() {return 'abc';},
         startsWith: String.prototype.startsWith
@@ -132,16 +128,12 @@ describe('String', function() {
       expect('abc'.endsWith('bc', -43)).to.not.be.ok;
       expect('abc'.endsWith('bc', -Infinity)).to.not.be.ok;
       expect('abc'.endsWith('bc', NaN)).to.not.be.ok;
-      expect((function() {
-        try { ''.endsWith.call(null, 'ull'); }
-        catch(e) { return true; }
-        return false;
-      })()).to.be.ok;
-      expect((function() {
-        try { ''.endsWith.call(undefined, 'ned'); }
-        catch(e) { return true; }
-        return false;
-      })()).to.be.ok;
+      expect(function() {
+        ''.endsWith.call(null, 'ull');
+      }).to.throw(TypeError);
+      expect(function() {
+        ''.endsWith.call(undefined, 'ned');
+      }).to.throw(TypeError);
 
       var myobj = {
         toString: function() { return 'abc'; },
