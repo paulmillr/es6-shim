7d9368c12940b0a5aadd772d444acf88640d6e96
Daniel Hug <daniel@hugwebdesign.com>
2013-07-21 07:34:11 -0700

    Move 'use strict'; to outer function

--- es6-shim.js
+++ es6-shim.js
@@ -4,6 +4,8 @@
 // https://github.com/paulmillr/es6-shim/
 
 (function(undefined) {
+  'use strict';
+
   var arePropertyDescriptorsSupported = function() {
     var attempt = function () {
       Object.defineProperty({}, 'x', {});
@@ -16,8 +18,6 @@
   };
 
   var main = function() {
-    'use strict';
-
     var globals = (typeof global === 'undefined') ? window : global;
     var global_isFinite = globals.isFinite;
     var supportsDescriptors = !!Object.defineProperty && arePropertyDescriptorsSupported();
