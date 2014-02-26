9349c1f20fb1dae9f63896780fb48cefdd1f1585
Jordan Harband <ljharb@gmail.com>
2014-02-25 23:36:00 -0800

    Make sure Object.keys does not throw.
    Per https://people.mozilla.org/~jorendorff/es6-draft.html#sec-object.keys - Object.keys should now accept non-objects, and coerce them to objects.

--- test/object.js
+++ test/object.js
@@ -1,6 +1,18 @@
 describe('Object', function() {
   var undefined;
 
+  describe('Object.keys()', function() {
+    it('works on strings', function() {
+      expect(Object.keys('foo')).to.eql(['0', '1', '2']);
+    });
+
+    it('works on other primitives', function() {
+      [true, false, undefined, NaN, 42, /a/g].forEach(function (item) {
+        expect(Object.keys(item)).to.eql([]);
+      });
+    });
+  });
+
   describe('Object.is()', function() {
     it('should compare regular objects correctly', function() {
       [null, undefined, [0], 5, 'str', {a: null}].map(function(item) {
