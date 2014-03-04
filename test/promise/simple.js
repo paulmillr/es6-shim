a38e82ecb5364b79620ec2faecb384bd0ee95628
C. Scott Ananian <cscott@cscott.net>
2014-03-04 11:41:45 -0500

    Regroup "simple" Promise test cases.
    The `promise-simple.js` tests were listed at top-level and as a result
    the mocha output was a bit confusing.

--- test/promise/simple.js
+++ test/promise/simple.js
@@ -4,15 +4,14 @@ var failIfThrows = function(done) {
   return function(e) { done(e || new Error()); };
 };
 
-describe("Easy-to-debug sanity check", function () {
-  specify("a fulfilled promise calls its fulfillment handler", function (done) {
+describe("Promise", function() {
+
+  specify("sanity check: a fulfilled promise calls its fulfillment handler", function (done) {
     Promise.resolve(5).then(function (value) {
       assert.strictEqual(value, 5);
     }).then(done, failIfThrows(done));
   });
-});
 
-describe("Self-resolution errors", function () {
   specify("directly resolving the promise with itself", function (done) {
     var resolvePromise;
     var promise = new Promise(function (resolve) { resolvePromise = resolve; });
@@ -28,23 +27,23 @@ describe("Self-resolution errors", function () {
       }
     ).then(done, failIfThrows(done));
   });
-});
 
-specify("Stealing a resolver and using it to trigger possible reentrancy bug (#83)", function () {
-  var stolenResolver;
-  function StealingPromiseConstructor(resolver) {
-    stolenResolver = resolver;
-    resolver(function () { }, function () { });
-  }
+    specify("Stealing a resolver and using it to trigger possible reentrancy bug (#83)", function () {
+        var stolenResolver;
+        function StealingPromiseConstructor(resolver) {
+            stolenResolver = resolver;
+            resolver(function () { }, function () { });
+        }
 
-  var iterable = {};
-  var atAtIterator = '@@iterator'; // on firefox, at least.
-  iterable[atAtIterator] = function () {
-    stolenResolver(null, null);
-    throw 0;
-  };
+        var iterable = {};
+        var atAtIterator = '@@iterator'; // on firefox, at least.
+        iterable[atAtIterator] = function () {
+            stolenResolver(null, null);
+            throw 0;
+        };
 
-  assert.doesNotThrow(function () {
-    Promise.all.call(StealingPromiseConstructor, iterable);
-  });
+        assert.doesNotThrow(function () {
+            Promise.all.call(StealingPromiseConstructor, iterable);
+        });
+    });
 });
