8008c9b2d06561d4f930f74bb9226dab25b0fec8
Sam Mikes <smikes@cubane.com>
2014-06-30 06:58:42 +0100

    test: use promises-es6-tests for additional promise tests

---
+++ test/promise/promises-es6.js
@@ -0,0 +1,20 @@
+// tests from promises-es6-tests
+/*global global, describe, require*/
+(function () {
+    "use strict";
+
+    describe("Promises/ES6 Tests", function () {
+
+        // an adapter that sets up global.Promise
+        // since it's already set up, empty functions will suffice
+        var adapter = {
+            defineGlobalPromise: function () {
+            },
+            removeGlobalPromise: function () {
+            }
+        };
+
+        require("promises-es6-tests").mocha(adapter);
+    });
+
+}());
