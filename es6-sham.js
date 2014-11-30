paulmillr/es6-shim

commit 156b5424f604e527d838859ca28f1be79af38da6

Use the same UMD in the sham as in the shim.

es6-sham.js
@@ -8,7 +8,22 @@
   * https://github.com/paulmillr/es6-shim/
   */
 
-(function (undefined) {
+// UMD (Universal Module Definition)
+// see https://github.com/umdjs/umd/blob/master/returnExports.js
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module.
+    define(factory);
+  } else if (typeof exports === 'object') {
+    // Node. Does not work with strict CommonJS, but
+    // only CommonJS-like enviroments that support module.exports,
+    // like Node.
+    module.exports = factory();
+  } else {
+    // Browser globals (root is window)
+    root.returnExports = factory();
+  }
+}(this, function (undefined) {
   'use strict';
 
   /*jshint evil: true */
@@ -103,11 +118,5 @@
     }());
 
   };
-
-  if (typeof define === 'function' && define.amd) {
-    define(main); // RequireJS
-  } else {
-    main(); // CommonJS and <script>
-  }
-}());
+}));
 

commit eddad462da4eca2867e130351665f43c84f4c902

Fixing an es5-sham UDM bug introduced in 156b5424f604e527d838859ca28f1be79af38da6

es6-sham.js
@@ -30,93 +30,90 @@
   var getGlobal = new Function('return this;');
   /*jshint evil: false */
 
-  var main = function () {
-    var globals = getGlobal();
-    var Object = globals.Object;
+  var globals = getGlobal();
+  var Object = globals.Object;
 
-    // NOTE:  This versions needs object ownership
-    //        beacuse every promoted object needs to be reassigned
-    //        otherwise uncompatible browsers cannot work as expected
-    //
-    // NOTE:  This might need es5-shim or polyfills upfront
-    //        because it's based on ES5 API.
-    //        (probably just an IE <= 8 problem)
-    //
-    // NOTE:  nodejs is fine in version 0.8, 0.10, and future versions.
-    (function () {
-      if (Object.setPrototypeOf) { return; }
+  // NOTE:  This versions needs object ownership
+  //        beacuse every promoted object needs to be reassigned
+  //        otherwise uncompatible browsers cannot work as expected
+  //
+  // NOTE:  This might need es5-shim or polyfills upfront
+  //        because it's based on ES5 API.
+  //        (probably just an IE <= 8 problem)
+  //
+  // NOTE:  nodejs is fine in version 0.8, 0.10, and future versions.
+  (function () {
+    if (Object.setPrototypeOf) { return; }
 
-      /*jshint proto: true */
-      // @author    Andrea Giammarchi - @WebReflection
-        // define into target descriptors from source
-      var copyDescriptors = function (target, source) {
-        getOwnPropertyNames(source).forEach(function (key) {
-          defineProperty(
-            target,
-            key,
-            getOwnPropertyDescriptor(source, key)
-          );
-        });
-        return target;
+    /*jshint proto: true */
+    // @author    Andrea Giammarchi - @WebReflection
+      // define into target descriptors from source
+    var copyDescriptors = function (target, source) {
+      getOwnPropertyNames(source).forEach(function (key) {
+        defineProperty(
+          target,
+          key,
+          getOwnPropertyDescriptor(source, key)
+        );
+      });
+      return target;
+    };
+    // used as fallback when no promotion is possible
+    var createAndCopy = function (origin, proto) {
+      return copyDescriptors(create(proto), origin);
+    };
+    var create = Object.create;
+    var defineProperty = Object.defineProperty;
+    var getPrototypeOf = Object.getPrototypeOf;
+    var getOwnPropertyNames = Object.getOwnPropertyNames;
+    var getOwnPropertyDescriptor = Object.getOwnPropertyDescriptor;
+    var proto = Object.prototype;
+    var set, setPrototypeOf;
+    try {
+      // this might fail for various reasons
+      // ignore if Chrome cought it at runtime
+      set = getOwnPropertyDescriptor(proto, '__proto__').set;
+      set.call({}, null);
+      // setter not poisoned, it can promote
+      // Firefox, Chrome
+      setPrototypeOf = function (origin, proto) {
+        set.call(origin, proto);
+        return origin;
       };
-      // used as fallback when no promotion is possible
-      var createAndCopy = function (origin, proto) {
-        return copyDescriptors(create(proto), origin);
-      };
-      var create = Object.create;
-      var defineProperty = Object.defineProperty;
-      var getPrototypeOf = Object.getPrototypeOf;
-      var getOwnPropertyNames = Object.getOwnPropertyNames;
-      var getOwnPropertyDescriptor = Object.getOwnPropertyDescriptor;
-      var proto = Object.prototype;
-      var set, setPrototypeOf;
-
-      try {
-        // this might fail for various reasons
-        // ignore if Chrome cought it at runtime
-        set = getOwnPropertyDescriptor(proto, '__proto__').set;
-        set.call({}, null);
-        // setter not poisoned, it can promote
-        // Firefox, Chrome
-        setPrototypeOf = function (origin, proto) {
-          set.call(origin, proto);
-          return origin;
-        };
-      } catch (e) {
-        // do one or more feature detections
-        set = {__proto__: null};
-        // if proto does not work, needs to fallback
-        // some Opera, Rhino, ducktape
+    } catch (e) {
+      // do one or more feature detections
+      set = {__proto__: null};
+      // if proto does not work, needs to fallback
+      // some Opera, Rhino, ducktape
+      if (set instanceof Object) {
+        setPrototypeOf = createAndCopy;
+      } else {
+        // verify if null objects are buggy
+        set.__proto__ = proto;
+        // if null objects are buggy
+        // nodejs 0.8 to 0.10
         if (set instanceof Object) {
-          setPrototypeOf = createAndCopy;
+          setPrototypeOf = function (origin, proto) {
+            // use such bug to promote
+            origin.__proto__ = proto;
+            return origin;
+          };
         } else {
-          // verify if null objects are buggy
-          set.__proto__ = proto;
-          // if null objects are buggy
-          // nodejs 0.8 to 0.10
-          if (set instanceof Object) {
-            setPrototypeOf = function (origin, proto) {
-              // use such bug to promote
-              origin.__proto__ = proto;
-              return origin;
-            };
-          } else {
-            // try to use proto or fallback
-            // Safari, old Firefox, many others
-            setPrototypeOf = function (origin, proto) {
-              // if proto is not null
-              return getPrototypeOf(origin) ?
-                // use __proto__ to promote
-                ((origin.__proto__ = proto), origin) :
-                // otherwise unable to promote: fallback
-                createAndCopy(origin, proto);
-            };
-          }
+          // try to use proto or fallback
+          // Safari, old Firefox, many others
+          setPrototypeOf = function (origin, proto) {
+            // if proto is not null
+            return getPrototypeOf(origin) ?
+              // use __proto__ to promote
+              ((origin.__proto__ = proto), origin) :
+              // otherwise unable to promote: fallback
+              createAndCopy(origin, proto);
+          };
         }
       }
-      Object.setPrototypeOf = setPrototypeOf;
-    }());
+    }
+    Object.setPrototypeOf = setPrototypeOf;
+  }());
 
-  };
 }));
 