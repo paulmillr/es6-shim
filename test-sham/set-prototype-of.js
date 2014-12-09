59be3cb4b83beaeaab91dec08ef4ed74918f6eaf
Jordan Harband <ljharb@gmail.com>
2014-12-08 22:02:09 -0800

    Use instanceOf chai matcher.

--- test-sham/set-prototype-of.js
+++ test-sham/set-prototype-of.js
@@ -3,21 +3,21 @@ describe('Object.setPrototypeOf(o, p)', function () {
 
   it('changes prototype to regular objects', function () {
     var obj = {a: 123};
-    expect(obj instanceof Object).to.equal(true);
+    expect(obj).to.be.an.instanceOf(Object);
     // sham requires assignment to work cross browser
     obj = Object.setPrototypeOf(obj, null);
-    expect(obj instanceof Object).to.equal(false);
+    expect(obj).not.to.be.an.instanceOf(Object);
     expect(obj.a).to.equal(123);
   });
 
   it('changes prototype to null objects', function () {
     var obj = Object.create(null);
     obj.a = 456;
-    expect(obj instanceof Object).to.equal(false);
+    expect(obj).not.to.be.an.instanceOf(Object);
     expect(obj.a).to.equal(456);
     // sham requires assignment to work cross browser
     obj = Object.setPrototypeOf(obj, {});
-    expect(obj instanceof Object).to.equal(true);
+    expect(obj).to.be.an.instanceOf(Object);
     expect(obj.a).to.equal(456);
   });
 
