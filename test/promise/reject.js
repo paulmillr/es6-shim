57af114440aad5faaa092f306451d28b995b0c07
Jordan Harband <ljharb@gmail.com>
2015-02-19 01:04:19 -0800

    Adding Promise.{reject, resolve} tests.

---
+++ test/promise/reject.js
@@ -0,0 +1,21 @@
+/*global describe, it, expect, setTimeout, assert, Promise */
+
+var failIfThrows = function (done) {
+  'use strict';
+  return function (e) { done(e || new Error()); };
+};
+
+describe('Promise.reject', function () {
+  'use strict';
+  it('should not be enumerable', function () {
+    expect(Object.getOwnPropertyDescriptor(Promise, 'reject')).to.have.property('enumerable', false);
+  });
+
+  it('should return a rejected promise', function (done) {
+    var value = {};
+    Promise.reject(value).then(failIfThrows(done), function (result) {
+      expect(result).to.equal(value);
+      done();
+    });
+  });
+});
---
+++ test/promise/resolve.js
@@ -0,0 +1,21 @@
+/*global describe, it, expect, setTimeout, assert, Promise */
+
+var failIfThrows = function (done) {
+  'use strict';
+  return function (e) { done(e || new Error()); };
+};
+
+describe('Promise.resolve', function () {
+  'use strict';
+  it('should not be enumerable', function () {
+    expect(Object.getOwnPropertyDescriptor(Promise, 'resolve')).to.have.property('enumerable', false);
+  });
+
+  it('should return a resolved promise', function (done) {
+    var value = {};
+    Promise.resolve(value).then(function (result) {
+      expect(result).to.equal(value);
+      done();
+    }, failIfThrows(done));
+  });
+});
