dc017747a5117a58914d1fd810b65389c61b8cf1
Jordan Harband <ljharb@gmail.com>
2015-04-28 11:57:26 -0700

    Fixing Map/Set invalid receiver error messages for WebKit.
    Per https://bugs.webkit.org/show_bug.cgi?id=144253

--- test/collections.js
+++ test/collections.js
@@ -294,7 +294,7 @@ describe('Collections', function () {
       });
 
       it('throws when called on a non-Map', function () {
-        var expectedMessage = /^(Method )?Map.prototype.entries called on incompatible receiver |^entries method called on incompatible /;
+        var expectedMessage = /^(Method )?Map.prototype.entries called on incompatible receiver |^entries method called on incompatible |^Cannot create a Map entry iterator for a non-Map object./;
         var nonMaps = [true, false, 'abc', NaN, new Set([1, 2]), { a: true }, [1], Object('abc'), Object(NaN)];
         nonMaps.forEach(function (nonMap) {
           expect(function () { return Map.prototype.entries.call(nonMap); }).to['throw'](TypeError, expectedMessage);
@@ -758,7 +758,7 @@ describe('Collections', function () {
       });
 
       it('throws when called on a non-Set', function () {
-        var expectedMessage = /^(Method )?Set.prototype.values called on incompatible receiver |^values method called on incompatible /;
+        var expectedMessage = /^(Method )?Set.prototype.values called on incompatible receiver |^values method called on incompatible |^Cannot create a Set value iterator for a non-Set object.$/;
         var nonSets = [true, false, 'abc', NaN, new Map([[1, 2]]), { a: true }, [1], Object('abc'), Object(NaN)];
         nonSets.forEach(function (nonSet) {
           expect(function () { return Set.prototype.values.call(nonSet); }).to['throw'](TypeError, expectedMessage);
