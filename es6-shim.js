2367e0953edd01ae9a5628e1f47cf14b0377a7d6
Jordan Harband <ljharb@gmail.com>
2015-07-30 16:53:26 -0700

    Avoid CSP errors in Chrome apps by using global var detection.
    Fixes #301.

--- es6-shim.js
+++ es6-shim.js
@@ -144,7 +144,15 @@
   }());
 
   /*jshint evil: true */
-  var getGlobal = new Function('return this;');
+  var getGlobal = function () {
+	// the only reliable means to get the global object is
+	// `Function('return this')()`
+	// However, this causes CSP violations in Chrome apps.
+    if (typeof self !== 'undefined') { return self; }
+    if (typeof window !== 'undefined') { return window; }
+    if (typeof global !== 'undefined') { return global; }
+	throw new Error('unable to locate global object');
+  };
   /*jshint evil: false */
 
   var globals = getGlobal();
