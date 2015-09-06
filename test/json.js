6e946493fc99547472b6fe2f6bdac13c73893a1f
Jordan Harband <ljharb@gmail.com>
2015-09-06 23:29:28 +0900

    Add more `JSON.stringify` tests wrt `Symbol`.

--- test/json.js
+++ test/json.js
@@ -1,24 +1,60 @@
-/* global describe, it, expect, require */
+/* global describe, it, xit, expect, require */
 describe('JSON', function () {
   var functionsHaveNames = (function foo() {}).name === 'foo';
   var ifFunctionsHaveNamesIt = functionsHaveNames ? it : xit;
   var hasSymbols = typeof Symbol === 'function' && typeof Symbol() === 'symbol';
   var ifSymbolsIt = hasSymbols ? it : xit;
+  var ifSymbolsDescribe = hasSymbols ? describe : xit;
 
   describe('.stringify()', function () {
+    ifSymbolsIt('serializes a Symbol to undefined', function () {
+      expect(JSON.stringify(Symbol())).to.eql(undefined);
+    });
+
     ifSymbolsIt('serializes Symbols in an Array to null', function () {
       expect(JSON.stringify([Symbol('foo')])).to.eql('[null]');
     });
 
-    ifSymbolsIt('skips symbol properties and values in an object', function () {
-      var obj = {};
+    ifSymbolsDescribe('skips symbol properties and values in an object', function () {
       var enumSym = Symbol('enumerable');
       var nonenum = Symbol('non-enumerable');
-      obj[enumSym] = true;
-      obj.sym = enumSym;
-      Object.defineProperty(obj, nonenum, { enumerable: false, value: true });
-      expect(Object.getOwnPropertySymbols(obj)).to.eql([enumSym, nonenum]);
-      expect(JSON.stringify(obj)).to.eql('{}');
+      var createObject = function () {
+        var obj = { a: 1 };
+        obj[enumSym] = true;
+        obj.sym = enumSym;
+        Object.defineProperty(obj, nonenum, { enumerable: false, value: true });
+        expect(Object.getOwnPropertySymbols(obj)).to.eql([enumSym, nonenum]);
+        return obj;
+      };
+
+      it('works with no replacer', function () {
+        var obj = createObject();
+        expect(JSON.stringify(obj)).to.equal('{"a":1}');
+        expect(JSON.stringify(obj, null, '|')).to.equal('{\n|"a": 1\n}');
+      });
+
+      it('works with a replacer function', function () {
+        var tuples = [];
+        var replacer = function (key, value) {
+          tuples.push([this, key, value]);
+          return value;
+        };
+        var obj = createObject();
+        expect(JSON.stringify(obj, replacer, '|')).to.equal('{\n|"a": 1\n}'); // populate `tuples` array
+        expect(tuples).to.eql([
+          [{ '': obj }, '', obj],
+          [obj, 'a', 1],
+          [obj, 'sym', enumSym]
+        ]);
+      });
+
+      it('works with a replacer array', function () {
+        var obj = createObject();
+        obj.foo = 'bar';
+        obj[Symbol.prototype.toString.call(enumSym)] = 'tricksy';
+        expect(JSON.stringify(obj, ['a', enumSym])).to.equal('{"a":1}');
+        expect(JSON.stringify(obj, ['a', enumSym], '|')).to.equal('{\n|"a": 1\n}');
+      });
     });
   });
 });
