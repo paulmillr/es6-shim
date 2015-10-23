8544a68ef66394c9b405806ca972a9b29337eab2
Jordan Harband <ljharb@gmail.com>
2015-10-22 23:29:45 -0700

    s/\t/    /g

--- es6-shim.js
+++ es6-shim.js
@@ -145,13 +145,13 @@
   }());
 
   var getGlobal = function () {
-	// the only reliable means to get the global object is
-	// `Function('return this')()`
-	// However, this causes CSP violations in Chrome apps.
+    // the only reliable means to get the global object is
+    // `Function('return this')()`
+    // However, this causes CSP violations in Chrome apps.
     if (typeof self !== 'undefined') { return self; }
     if (typeof window !== 'undefined') { return window; }
     if (typeof global !== 'undefined') { return global; }
-	throw new Error('unable to locate global object');
+    throw new Error('unable to locate global object');
   };
 
   var globals = getGlobal();
